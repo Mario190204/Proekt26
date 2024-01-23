a48cd12bc6cdbd98db8bf216c013685cdb52fdf9
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 13:18:47 +0100

    Proekt26

---
+++ src/Vehicle/MyClass.java
@@ -0,0 +1,10 @@
+package Vehicle;
+
+public class MyClass {
+
+	 public static void main(String[] args) {
+	        Vehicle myVehicle = new Vehicle(); // Create an instance of the Vehicle class
+	        myVehicle.horn(); // Call the horn() method through the reference
+	        System.out.println(myVehicle); // Print the string representation using toString()
+	    }
+	}
---
+++ src/Vehicle/Vehicle.java
@@ -0,0 +1,12 @@
+package Vehicle;
+
+public class Vehicle {
+	public void horn() {
+        System.out.println("Beep");
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
